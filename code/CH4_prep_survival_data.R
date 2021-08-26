5bd610d16b3c86f4f1a0968e5313380713694640
scottfjennings <scott.f.jennings@gmail.com>
2021-08-26 10:20:30 -0700

    updated for code based inp creation

--- code/CH4_prep_survival_data.R
+++ code/CH4_prep_survival_data.R
@@ -30,7 +30,6 @@ setwd(here("mark_output"))
 # the following script is adapted for my data from appendix C of the Mark book
 
 # CJS analysis of penguin chick survival
-# .inp file is created by hand in a spreadsheet editor, then saved at a .txt, then saved as a .inp
 
 # Import data (all_covs_groups.inp) and convert it from the MARK inp file format to the \textbf{RMark}
 # format using the function convert.inp 
@@ -38,9 +37,10 @@ setwd(here("mark_output"))
 # This structure is defined with the group.df argument of convert.inp. 
 ##C:/Users/jenninsc/Documents/THESIS/Data/resighting_survival/RMark_analysis/
 
-## !!!NOTE 8/25/21-  did.cr in all_covs_sex_groups.inp is NOT correct. I am currently dealing with this downstream of reading in all_covs_sex_groups.inp, rather than remaking it.
+## !!!NOTE 8/26/21- inp file now almost entirely created by code, including in.cr fields. See CH4_make_inp.R for code and notes 
 
-penguins=convert.inp(here("data/all_covs_sex_groups.inp"), 
+
+penguins=convert.inp(here("data/mark_in.inp"), 
 					group.df=data.frame(sex=rep(c("Male","Female"),2), SEASON=c(rep("1213",2),rep("1314",2))), 
 					covariates=c("dayold50", "dayold51", "dayold52", "dayold53", "dayold54", "dayold55", "dayold56", 
 					 "dayold57", "dayold58", "dayold59", "dayold60", "dayold61", "dayold62", "dayold63", "dayold64", 
@@ -48,9 +48,14 @@ penguins=convert.inp(here("data/all_covs_sex_groups.inp"),
 					 "dayold73", "dayold74", "dayold75", "dayold76", "dayold77", "dayold78", "dayold79", "dayold80",
 					 "dayold81", "dayold82", "dayold83", "dayold84", "dayold85", "dayold86", "dayold87", "dayold88", 
 					 "dayold89", "dayold90", "dayold91", "dayold92", "dayold93", "dayold94", "dayold95", "dayold96",
-					 "dayold97", "dayold98", "av.food", "av.trip.length", "cr.age", "fail.age", "weight", 
-					"flipper", "tibiotar", "CH_B", "CH_S", "mean.N", "cr.mass", 
-					"res.htch", "cr.flip", "cr.tib", "did.cr", "fate.age" ),use.comments=FALSE) 
+					 "dayold97", "dayold98","in.cr50", "in.cr51", "in.cr52", "in.cr53", "in.cr54", "in.cr55", "in.cr56", 
+					 "in.cr57", "in.cr58", "in.cr59", "in.cr60", "in.cr61", "in.cr62", "in.cr63", "in.cr64", 
+					 "in.cr65", "in.cr66", "in.cr67", "in.cr68", "in.cr69", "in.cr70", "in.cr71", "in.cr72",
+					 "in.cr73", "in.cr74", "in.cr75", "in.cr76", "in.cr77", "in.cr78", "in.cr79", "in.cr80",
+					 "in.cr81", "in.cr82", "in.cr83", "in.cr84", "in.cr85", "in.cr86", "in.cr87", "in.cr88", 
+					 "in.cr89", "in.cr90", "in.cr91", "in.cr92", "in.cr93", "in.cr94", "in.cr95", "in.cr96",
+					 "in.cr97", "in.cr98", "cr.age", "fail.age", "cr.mass", "resid.hatch", "cr.flip", "cr.tib"), use.comments = TRUE) 
+
 
 		 
 ##	setting NA values to mean of that field					
@@ -67,58 +72,6 @@ penguins$mean.N[is.na(penguins$mean.N)]=10.76
 #penguins$cr.age[is.na(penguins$cr.age)]=penguins$fail.age		#20
 
 #write.csv(penguins, "resighting_survival/temp_penguins.csv")
-
-## add time-varying covariates for creched or not
-penguins$in.cr50=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold50<penguins$cr.age,0,1))
-penguins$in.cr51=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold51<penguins$cr.age,0,1))
-penguins$in.cr52=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold52<penguins$cr.age,0,1))
-penguins$in.cr53=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold53<penguins$cr.age,0,1))
-penguins$in.cr54=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold54<penguins$cr.age,0,1))
-penguins$in.cr55=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold55<penguins$cr.age,0,1))
-penguins$in.cr56=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold56<penguins$cr.age,0,1))
-penguins$in.cr57=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold57<penguins$cr.age,0,1))
-penguins$in.cr58=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold58<penguins$cr.age,0,1))
-penguins$in.cr59=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold59<penguins$cr.age,0,1))
-penguins$in.cr60=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold60<penguins$cr.age,0,1))
-penguins$in.cr61=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold61<penguins$cr.age,0,1))
-penguins$in.cr62=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold62<penguins$cr.age,0,1))
-penguins$in.cr63=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold63<penguins$cr.age,0,1))
-penguins$in.cr64=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold64<penguins$cr.age,0,1))
-penguins$in.cr65=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold65<penguins$cr.age,0,1))
-penguins$in.cr66=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold66<penguins$cr.age,0,1))
-penguins$in.cr67=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold67<penguins$cr.age,0,1))
-penguins$in.cr68=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold68<penguins$cr.age,0,1))
-penguins$in.cr69=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold69<penguins$cr.age,0,1))
-penguins$in.cr70=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold70<penguins$cr.age,0,1))
-penguins$in.cr71=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold71<penguins$cr.age,0,1))
-penguins$in.cr72=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold72<penguins$cr.age,0,1))
-penguins$in.cr73=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold73<penguins$cr.age,0,1))
-penguins$in.cr74=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold74<penguins$cr.age,0,1))
-penguins$in.cr75=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold75<penguins$cr.age,0,1))
-penguins$in.cr76=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold76<penguins$cr.age,0,1))
-penguins$in.cr77=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold77<penguins$cr.age,0,1))
-penguins$in.cr78=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold78<penguins$cr.age,0,1))
-penguins$in.cr79=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold79<penguins$cr.age,0,1))
-penguins$in.cr80=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold80<penguins$cr.age,0,1))
-penguins$in.cr81=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold81<penguins$cr.age,0,1))
-penguins$in.cr82=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold82<penguins$cr.age,0,1))
-penguins$in.cr83=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold83<penguins$cr.age,0,1))
-penguins$in.cr84=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold84<penguins$cr.age,0,1))
-penguins$in.cr85=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold85<penguins$cr.age,0,1))
-penguins$in.cr86=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold86<penguins$cr.age,0,1))
-penguins$in.cr87=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold87<penguins$cr.age,0,1))
-penguins$in.cr88=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold88<penguins$cr.age,0,1))
-penguins$in.cr89=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold89<penguins$cr.age,0,1))
-penguins$in.cr90=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold90<penguins$cr.age,0,1))
-penguins$in.cr91=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold91<penguins$cr.age,0,1))
-penguins$in.cr92=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold92<penguins$cr.age,0,1))
-penguins$in.cr93=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold93<penguins$cr.age,0,1))
-penguins$in.cr94=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold94<penguins$cr.age,0,1))
-penguins$in.cr95=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold95<penguins$cr.age,0,1))
-penguins$in.cr96=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold96<penguins$cr.age,0,1))
-penguins$in.cr97=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold97<penguins$cr.age,0,1))
-penguins$in.cr98=ifelse(penguins$did.cr==0, 0, ifelse(penguins$dayold98<penguins$cr.age,0,1))
- 
 
 penguins$never.cr=1-penguins$did.cr
 penguins$did.cr=as.numeric(penguins$did.cr)
