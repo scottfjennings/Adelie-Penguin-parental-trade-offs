c4c997dc57307685158406a95321743abd925a91
scottfjennings <scott.f.jennings@gmail.com>
2021-08-27 15:09:54 -0700

    notes and comparison for 13 chicks that were mysteriously excluded from thesis analysis

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # thesis_ch4_analysis
 code, data, etc. for analysis of Adelie Penguin survival; chapter 4 of thesis
+
+
+
+The analysis has 2 major parts, each with multiple sub-parts
+
+1. Growth rates and creche timing
+1) how do mass, flipper, and tibiotarsus growth rates predict age at crèche; 
+2) how does mass growth rate predict mass at creching;
+3) how does flipper growth rate predict flipper length at creching; and 
+4) how does tibiotarsus growth rate predict tibiotarsus length at creching
+
+2. Survival
+1) can survival across the entire provisioning period be predicted by mass, flipper or tibiotarsus growth rates; and 
+2) can survival during the crèche period be predicted by age or size at crèching
+
+
+
+Part 1 is accomplished with a series of linear mixed effects models via the code in the following files:
+CH4_growth_creche.R
+
+Part 2 is accomplished with Cormack-Jolly-Seber (CJS) survival models via the code in the following files:
+Rmark_penguins.R
+Rmark_penguins_sex_groups.R
+
+
+
+
+
+8/27/21: there are 13 chicks in the new inp that were excluded from the thesis analysis. I have examined here("data/ana_table_full") and can't find anything that separates these chicks from all the others. Also searched through all the folders in thesis_data_work and couldn't find any notes indicating why these chicks were excluded. I am currently running the analysis with these chicks.
--- code/CH4_make_inp.R
+++ code/CH4_make_inp.R
@@ -70,3 +70,37 @@ write.csv(mark_inp, here("data/mark_in.csv"), row.names = FALSE)
 
 # open the resulting file in notepad++, delete the first line that has only "ch", then do replace all to get rid of all the ".
 # finally, do save as and include ".inp" to the end of the file name to save as .inp
+
+##############################
+
+# for comparison, read in inp file used for thesis analysis
+
+# 8/27/21: there are 13 chicks in the new inp that were excluded from the thesis analysis. I have examined here("data/ana_table_full") and can't find anything that separates these chicks from all the others. Also searched through all the folders in thesis_data_work and couldn't find any notes indicating why these chicks were excluded. I am currently running the analysis with these chicks.
+
+penguins_thesis = convert.inp(here("data/all_covs_sex_groups.inp"), group.df=data.frame(sex=rep(c("Male","Female"),2), SEASON=c(rep("1213",2),rep("1314",2))), covariates=c("dayold50", "dayold51", "dayold52", "dayold53", "dayold54", "dayold55", "dayold56",  "dayold57", "dayold58", "dayold59", "dayold60", "dayold61", "dayold62", "dayold63", "dayold64",  "dayold65", "dayold66", "dayold67", "dayold68", "dayold69", "dayold70", "dayold71", "dayold72", "dayold73", "dayold74", "dayold75", "dayold76", "dayold77", "dayold78", "dayold79", "dayold80", "dayold81", "dayold82", "dayold83", "dayold84", "dayold85", "dayold86", "dayold87", "dayold88", "dayold89", "dayold90", "dayold91", "dayold92", "dayold93", "dayold94", "dayold95", "dayold96", "dayold97", "dayold98", "av.food", "av.trip.length", "cr.age", "fail.age", "weight", "flipper", "tibiotar", "CH_B", "CH_S", "mean.N", "cr.mass", "res.htch", "cr.flip", "cr.tib", "did.cr", "fate.age" ),use.comments=FALSE) 
+ 
+
+penguins=convert.inp(here("data/mark_in.inp"), 
+					group.df=data.frame(sex=rep(c("Male","Female"),2), SEASON=c(rep("1213",2),rep("1314",2))), 
+					covariates=c("dayold50", "dayold51", "dayold52", "dayold53", "dayold54", "dayold55", "dayold56", 
+					 "dayold57", "dayold58", "dayold59", "dayold60", "dayold61", "dayold62", "dayold63", "dayold64", 
+					 "dayold65", "dayold66", "dayold67", "dayold68", "dayold69", "dayold70", "dayold71", "dayold72",
+					 "dayold73", "dayold74", "dayold75", "dayold76", "dayold77", "dayold78", "dayold79", "dayold80",
+					 "dayold81", "dayold82", "dayold83", "dayold84", "dayold85", "dayold86", "dayold87", "dayold88", 
+					 "dayold89", "dayold90", "dayold91", "dayold92", "dayold93", "dayold94", "dayold95", "dayold96",
+					 "dayold97", "dayold98","in.cr50", "in.cr51", "in.cr52", "in.cr53", "in.cr54", "in.cr55", "in.cr56", 
+					 "in.cr57", "in.cr58", "in.cr59", "in.cr60", "in.cr61", "in.cr62", "in.cr63", "in.cr64", 
+					 "in.cr65", "in.cr66", "in.cr67", "in.cr68", "in.cr69", "in.cr70", "in.cr71", "in.cr72",
+					 "in.cr73", "in.cr74", "in.cr75", "in.cr76", "in.cr77", "in.cr78", "in.cr79", "in.cr80",
+					 "in.cr81", "in.cr82", "in.cr83", "in.cr84", "in.cr85", "in.cr86", "in.cr87", "in.cr88", 
+					 "in.cr89", "in.cr90", "in.cr91", "in.cr92", "in.cr93", "in.cr94", "in.cr95", "in.cr96",
+					 "in.cr97", "in.cr98", "cr.age", "fail.age", "cr.mass", "res.htch", "cr.flip", "cr.tib"), use.comments = TRUE) 
+
+peng_comp <- anti_join(penguins %>% rownames_to_column("chick_id") %>% select(chick_id, ch),
+                       penguins_thesis %>% select(ch)) %>% 
+  mutate(excluded = TRUE,
+         chick_id = trimws(chick_id)) %>% 
+  select(-ch) %>% 
+  full_join(ana_table)
+
+
