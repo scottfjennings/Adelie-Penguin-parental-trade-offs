3c70fcceaf8c2e1ece40dfa795cf809dd5259ee2
scottfjennings <scott.f.jennings@gmail.com>
2021-09-08 09:32:41 -0700

    housekeeping and remove gof testing

--- code/survival3_overall_survival.r
+++ code/survival3_overall_survival.r
@@ -1,3 +1,6 @@
+
+
+
 #need to tell R that Mark is not stored in C:\Program Files
 
 #ACR computer
@@ -17,27 +20,53 @@ setwd(here("mark_output"))
 
 
 
+# Nov 1 is season day 1 both years of this study. so begin.time = 50 makes analysis start on
+# as.Date("2012-11-01") + 49
+# "2012-12-20"
+# as.Date("2013-11-01") + 49
+# "2013-12-20"
 
-# goes to root folder for project -- not sure why need to output these so commented out for now
-# export.MARK(penguins.process, "processed_penguins_ch")
-# export.chdata(penguins.process, filename="penguin", replace=TRUE)
+# mean hatch day across both years is Dec 19, so start models on mean 1 day old
 
-## GOF testing ----
-# GOF testing with RELEASE requires no individual covariates and 
-# requires dots in the capture history to be replaced with 0. dot -> 0 is supposed to happen automatically, but for some reason isn't, so doing it manually
-peng.for.gof=subset(penguins, select=c("ch", "freq", "sex", "SEASON"))
+# the following script is adapted for my data from appendix C of the Mark book
 
-peng.for.gof <- peng.for.gof %>% 
-  mutate(ch = gsub("\\.", "0", ch))
+# CJS analysis of penguin chick survival
 
-peng.for.gof.proc=process.data(peng.for.gof,model="CJS",groups=c("SEASON", "sex"))
+# Import data (all_covs_groups.inp) and convert it from the MARK inp file format to the \textbf{RMark}
+# format using the function convert.inp 
+# It is defined with 4 groups: Males in 1213, females in 1213, males in 1314, and females in 1314
+# This structure is defined with the group.df argument of convert.inp. 
+##C:/Users/jenninsc/Documents/THESIS/Data/resighting_survival/RMark_analysis/
 
-release.gof(peng.for.gof.proc)
-# note, release.gof creates mxxx.tmp file
-# divide total Chi.square by total df to get estimate of c-hat
-# > 179.5428/144
-# [1] 1.246825
-c_hat = round(179.5428/144, 2)
+## !!!NOTE 8/26/21- inp file now almost entirely created by code, including in.cr fields. See CH4_make_inp.R for code and notes 
+
+
+penguins=convert.inp(here("data/mark_in.inp"), 
+					group.df=data.frame(sex=rep(c("Male","Female"),2), SEASON=c(rep("1213",2),rep("1314",2))), 
+					covariates=c("dayold50", "dayold51", "dayold52", "dayold53", "dayold54", "dayold55", "dayold56", 
+					 "dayold57", "dayold58", "dayold59", "dayold60", "dayold61", "dayold62", "dayold63", "dayold64", 
+					 "dayold65", "dayold66", "dayold67", "dayold68", "dayold69", "dayold70", "dayold71", "dayold72",
+					 "dayold73", "dayold74", "dayold75", "dayold76", "dayold77", "dayold78", "dayold79", "dayold80",
+					 "dayold81", "dayold82", "dayold83", "dayold84", "dayold85", "dayold86", "dayold87", "dayold88", 
+					 "dayold89", "dayold90", "dayold91", "dayold92", "dayold93", "dayold94", "dayold95", "dayold96",
+					 "dayold97", "dayold98","in.cr50", "in.cr51", "in.cr52", "in.cr53", "in.cr54", "in.cr55", "in.cr56", 
+					 "in.cr57", "in.cr58", "in.cr59", "in.cr60", "in.cr61", "in.cr62", "in.cr63", "in.cr64", 
+					 "in.cr65", "in.cr66", "in.cr67", "in.cr68", "in.cr69", "in.cr70", "in.cr71", "in.cr72",
+					 "in.cr73", "in.cr74", "in.cr75", "in.cr76", "in.cr77", "in.cr78", "in.cr79", "in.cr80",
+					 "in.cr81", "in.cr82", "in.cr83", "in.cr84", "in.cr85", "in.cr86", "in.cr87", "in.cr88", 
+					 "in.cr89", "in.cr90", "in.cr91", "in.cr92", "in.cr93", "in.cr94", "in.cr95", "in.cr96",
+					 "in.cr97", "in.cr98", "res.htch", "cr.age", "fail.age", "mass.gr", "tib.gr", "flip.gr", "cr.mass", "cr.flip", "cr.tib"), use.comments = TRUE) 
+
+
+
+# Next create the processed dataframe and the design data. We’ll use a group 
+# variable for colony so it can be used in the set of models for Phi. Factor 
+# variables (covariates with a small finite set of values) are best handled by using 
+# them to define groups in the data. 
+# need to set begin.time to 51 because RMark considers the first dayold to be the first release dayold, but really I want to consider recaptures beginning on dayold 52.
+penguins.process = process.data(penguins, model = "CJS", groups = c("SEASON", "sex"), begin.time = 50) 
+penguins.ddl = make.design.data(penguins.process)
+
 
 # fitting models ----
 ##	making models without mark.wrapper
@@ -225,17 +254,13 @@ phi_step2=collect.models(lx=c("Phi.SEASON_hatch", "Phi.SEASON_hatch_t", "Phi.SEA
                               #
                               "Phi.SEASON.hatch", "Phi.SEASON.hatch_t", "Phi.SEASON.hatch_T", "Phi.SEASON.hatch_TT", "Phi.SEASON.hatch_lnT", "Phi.SEASON.hatch_t_incr", "Phi.SEASON.hatch_t_age", "Phi.SEASON.hatch_t.incr", "Phi.SEASON.hatch_t.age"))
 
+saveRDS(phi_step2, here("fitted_models/phi_step2"))
 
-
-
-saveRDS(phi_step2, here("fitted_models/phi_step2_neo"))
-
-phi_step2 <- readRDS(here("fitted_models/phi_step2"))
-
-
-#----
+# model averaging ----
 
 step2_mod_av = model.average(phi_step2, "Phi", vcv=TRUE, drop = FALSE)
+
+phi_step2 <- readRDS(here("fitted_models/phi_step2"))
 
 saveRDS(step2_mod_av, here("fitted_models/step2_mod_av"))
 
